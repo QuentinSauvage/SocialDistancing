d25fe86c1407a4b894ebf82ab644dc9b4882b881
QuentinSauvage <sauvage.quentin62@gmail.com>
2020-04-12 16:23:01 +0200

    animations, camera and better collisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] float _speed = 10;
 	Rigidbody2D _rigidbody2D;
 	PlayerActions _playerActions;
+	Animator _animator;
 	Vector2 _movementAction;
 
 	private void Awake()
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour
 		_rigidbody2D = GetComponent<Rigidbody2D>();
 		_playerActions = new PlayerActions();
 		_playerActions.PlayerMovement.Move.performed += ctx => _movementAction = ctx.ReadValue<Vector2>();
+		_animator = GetComponent<Animator>();
 	}
 
 	void Start()
@@ -31,6 +33,19 @@ public class PlayerController : MonoBehaviour
 		float x = _movementAction.x * _speed * Time.deltaTime;
 		float y = _movementAction.y * _speed * Time.deltaTime;
 		Vector3 move = new Vector3(x, y, 0);
+
+		if(y != 0)
+		{
+			_animator.SetFloat("MoveX", 0);
+			_animator.SetFloat("MoveY", y);
+		}
+		else if(x != 0)
+		{
+			_animator.SetFloat("MoveX", x);
+			_animator.SetFloat("MoveY", 0);
+		}
+		_animator.SetFloat("Speed", move.magnitude);
+		Debug.Log(move.magnitude);
 		move += transform.position;
 
 		_rigidbody2D.MovePosition(move);
